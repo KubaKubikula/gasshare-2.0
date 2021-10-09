5430f1d7eb87fcdb10f0aa47231b774ec029730d
Kubakubikula <jakub.zient@gmail.com>
2021-10-09 20:29:56 +0100

    remove snippets

--- api/api/settings.py
+++ api/api/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'snippets.apps.SnippetsConfig',
     'drives.apps.DrivesConfig',
     'users.apps.UsersConfig',
     'corsheaders',
