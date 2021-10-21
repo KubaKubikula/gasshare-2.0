306ecb962492fb62feacc5040d435872191429fa
Kubakubikula <jakub.zient@gmail.com>
2021-10-21 18:51:25 +0200

    repair parse errror

--- api/drives/serializers.py
+++ api/drives/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from drives.models import Drive
 
-class DriveSerializer(serializers.Serializer):
-    hitchhike = serializers.BooleanField(False)
-    
+class DriveSerializer(serializers.Serializer): 
     class Meta:
         model = Drive
         fields = ['id', 'created', 'drive_from', 'drive_to','time', 'hitchhike']
