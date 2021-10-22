5e6c4978323b176d1ad671e108570c96cab7b7b4
Kubakubikula <jakub.zient@gmail.com>
2021-10-22 22:00:06 +0200

    drives

--- frontend/src/components/pages/Drives.js
+++ frontend/src/components/pages/Drives.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import '../../css/App.css';
 
-
-
-
 class Drives extends Component {
     
     constructor(props) {
@@ -13,24 +10,43 @@ class Drives extends Component {
         }
     }
 
+    const moviesItems = movies !== null ? movies.map(movie => (
+            <Link to={`/detail/${movie.id}`} className="main-carousel__item-link" key={movie.id}>
+                <div className="main-carousel__item">
+                    <img
+                        src={movie.backdrop_path !== null ? `http://image.tmdb.org/t/p/w342/${movie.backdrop_path}` : ''}
+                        className="main-carousel__item-img"
+                        alt={movie.original_title ? movie.original_title : movie.original_name}/>
+                    <h4 className="main-carousel__item-title">
+                        {movie.original_title ? movie.original_title : movie.original_name}
+                    </h4>
+                </div>
+            </Link>
+        )) :
+    null;
+
     componentDidMount() {
-        this.state.drives = this.getDrives();
+        this.getDrives();
     }
 
-    getDrives() {
+    async getDrives() {
+
         const requestOptions = {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
             };
         
-        fetch('http://localhost:8000/drives/', requestOptions)
+        await fetch('http://localhost:8000/drives/', requestOptions)
             .then(response => {
-                console.log(response);
-                return response;
+                response.json().then(data => {
+                    this.state.drives = data;
+                });
+                
+                
         }) 
     }    
 
-    render() {   
+    render() {
         return <div>{this.state.drives}</div>  
     }
 }
