ea97ef0f58d49d04050be49a1e42774c7239caf7
Kubakubikula <jakub.zient@gmail.com>
2021-10-25 17:41:26 +0200

    test is working
    exit
    ;
    !
    quit

--- api/users/tests.py
+++ api/users/tests.py
@@ -4,8 +4,8 @@ from passlib.hash import bcrypt_sha256
 
 class UserTestCase(TestCase):
     def setUp(self):
-        User.hitchhiker_objects.create(email="jakub.zient@gmail.com", password=bcrypt_sha256.hash("heslo"))
+        User.hitchhiker_objects.create(email="jakub.zient@gmmail.com", password=bcrypt_sha256.hash("heslo"))
 
     def test_animals_can_speak(self):
-        user = User.hitchhiker_objects.get_user(email="jakub.zient@gmail.com", password="heslo")
-        self.assertEqual("jakub.zient@gmail.com", "jakub.zient@gmail.com")
+        user = User.hitchhiker_objects.get_user(email="jakub.zient@gmmail.com", password="heslo")
+        self.assertEqual("jakub.zient@gmmail.com", "jakub.zient@gmmail.com")
