14c2cd05323c00865356c459c74a7eb49e9dbe01
Kubakubikula <jakub.zient@gmail.com>
2021-10-25 14:31:38 +0200

    tests

--- api/users/tests.py
+++ api/users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from users.models import User
+from passlib.hash import bcrypt_sha256
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.hitchhiker_objects.create(email="jakub.zient@gmail.com", password=bcrypt_sha256.hash("heslo"))
+
+    def test_animals_can_speak(self):
+        user = User.hitchhiker_objects.get_user(email="jakub.zient@gmail.com", password="heslo")
+        
+        self.assertEqual(user.email, "jakub.zient@gmail.com")
