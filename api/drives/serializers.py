150e2218ee54b12f4ac4dd14d719c150d89cce02
Kubakubikula <jakub.zient@gmail.com>
2021-10-27 20:36:41 +0200

    pauza

--- api/drives/serializers.py
+++ api/drives/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from drives.models import Drive
-
+import logging
 class DriveSerializer(serializers.ModelSerializer): 
 
     class Meta:
         model = Drive
         db_table = 'drives'
         fields = ['id', 'created', 'drive_from', 'drive_to','time', 'hitchhike', 'user_id']
-        
+    
+    def save(self):
+        drive = Drive(self.validated_data)
+
+        logging.warning(self.validated_data)
+
+        return True
