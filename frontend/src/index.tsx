65e10dd89bc2137f885a342ef422ace7b6f828da
Kubakubikula <jakub.zient@gmail.com>
2021-11-05 19:06:06 +0100

    possible solution fo login maybe ?

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const checkLoginStatus = async () => {
+  const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: { 'token' : localStorage.getItem('token') }
+  };
+
+  axios
+      .post("http://127.0.0.1:8000/loggedin/", requestOptions)
+      .then(response => {
+      if (
+          response.data.loggedIn === "true"
+      ) {
+          return true;
+      } else {
+          return false
+      }
+      })
+      .catch(error => {
+      console.log("check login error", error);
+  });
+}
+
+checkLoginStatus().then(function() {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  }
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
